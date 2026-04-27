135c55abc8c04f39d20831ca85aefc6edec35f68
NwoguGlory1 <glorynwogu18@gmail.com>
2026-04-27 16:01:51 +0100

    Edited Student model, home page of http://127.0.0.1:8000/api/ is rendering now

--- StudentAPI/Student/students/models.py
+++ StudentAPI/Student/students/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Student(models.Model):
     name  = models.CharField(max_length=200)
-    age = models.DateField(max_length=20)
+    age = models.IntegerField(max_length=20)
     email = models.EmailField(unique=True)
     # department = models.ForeignKey(Department, on_delete=models.CASCADE)
     def __str__(self):
