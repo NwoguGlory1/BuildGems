7345ce187a3c8383f569e7fa90320748154106e0
NwoguGlory1 <glorynwogu18@gmail.com>
2026-05-20 20:01:35 +0100

    anual Filtering by id, name, age works using the get_queryset() method

--- StudentAPI/Student/students/views.py
+++ StudentAPI/Student/students/views.py
@@ -4,6 +4,7 @@
 from rest_framework.decorators import action
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework import filters
 import django_filters.rest_framework
 
 from .models import Student
@@ -14,29 +15,43 @@ class StudentViewSet(viewsets.ModelViewSet): #ModelViewSet provides CRUD
     queryset = Student.objects.all() #defines the set of objects available via the API.
     serializer_class = StudentSerializer #tells DRF how serialize/deserialize the Student data
     # ModelViewSet automatically provides list, retrieve, create, update and destroy actions.
-    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    # filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
 
-    # using the special DRF config attribute, filterset_fields to filter:
-    filterset_fields = ['id', 'name', 'age']
-# example: {{student_base_url}}/students/?id=3&name=KesingKaesing BuildGems
-# {{student_base_url}}/students/?id=3&name=KesingKaesing BuildGems&age=87
+    # Automatic filtering using the special DRF config attribute, filterset_fields to filter:
+    # filterset_fields = ['id', 'name', 'age']
+    # example: {{student_base_url}}/students/?id=3&name=KesingKaesing BuildGems
+    # {{student_base_url}}/students/?id=3&name=KesingKaesing BuildGems&age=87
 
+    # Manual filtering using .get_queryset()
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        name = self.request.query_params.get('name') 
+        id = self.request.query_params.get('id') 
+        age = self.request.query_params.get('age') 
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        if id:
+           queryset = queryset.filter(id=id)
+        if age:   
+            queryset = queryset.filter(age__gte=age)
+        return queryset
 
     @action(detail=False, methods=['GET'])
-    # Use detail=True when working with ONE object, depends on pk   
+    # Use detail=True when working with ONE object, depends on pk which is for path params  
     def top_students(self, request):
         # custom action logic for GET request
         students = Student.objects.filter(age__gte=100)
-  
         serializer = self.get_serializer(students, many=True)
-       
         data = serializer.data  #After creating a serializer, you get serializer.data
-        # print(type(data))
+        # print(type(data)) test this
 
         # DRF received the response object & calls renderer automatically
         # CustomRenderer is global from settings.py, so you dont need to call it
         return Response(serializer.data)
+    
 
+
+            
 
 
     # @action(detail=False, methods=['POST'])
