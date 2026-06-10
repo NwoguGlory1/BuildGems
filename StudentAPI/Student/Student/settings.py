2ff124560e57d9b8169ef0245df12ca2071a8250
NwoguGlory1 <glorynwogu18@gmail.com>
2026-06-10 10:48:48 +0100

    Connected student app to mysqldatabase and CRUD works very well alongside pagination and versioning but DELETE error format is not right

--- StudentAPI/Student/Student/settings.py
+++ StudentAPI/Student/Student/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = "Student.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "student_db",
+        "USER": "student_user",
+        "PASSWORD": "20141893613",
+        "HOST": "localhost",
+        "PORT": "3306",
+
     }
 }
 
