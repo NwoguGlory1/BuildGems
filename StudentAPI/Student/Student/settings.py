c8d4134a8b6d0f509c624cdb97561a7e51f27344
NwoguGlory1 <glorynwogu18@gmail.com>
2026-07-06 18:22:03 +0100

    installed django-redis and redis response of PONG shows that it is ready

--- StudentAPI/Student/Student/settings.py
+++ StudentAPI/Student/Student/settings.py
@@ -164,7 +164,7 @@ REST_FRAMEWORK = {
 
 #JWT config - customize token behaviour
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True, 
